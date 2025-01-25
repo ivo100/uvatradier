d4d1baaa7b3ec6f4656aa1216826ddb272eb54d4
ivo100 <ivostoy@gmail.com>
2025-01-24 19:35:24 -0800

    setup

--- setup.py
+++ setup.py
@@ -12,15 +12,16 @@ setup(
 	long_description=long_description,
 	long_description_content_type='text/markdown',
 	url='https://github.com/thammo4/uvatradier',
+	python_requires=">=3.10",
 	packages=find_packages(),
 	project_urls={'Bug Tracker':'https://github.com/thammo4/uvatradier/issues'},
 	keywords='tradier finance api',
     install_requires=[
-        'requests>=2.0',
+        'requests>=2.25',
 		'python-dotenv>=1.0',
-		'pandas>=1.0',
-        'matplotlib>=3.0',
-		"websockets~=10.1",
+		'pandas>=2.0',
+        'matplotlib>=3.5',
+		"websockets>=10.3",
         'asyncio'  			# asyncio is included in the standard library for Python 3.7 and later - unneeded if using these versions.
     ]
 )
